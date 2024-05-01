cf70485bbb166661dfec285195110b0aafb2f447
MarcosArgandona <m.argando@gmail.com>
2024-05-01 01:08:28 -0400

    se crean respositorios y estructuras principales

---
+++ assets/js/index.js
@@ -0,0 +1,26 @@
+import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
+
+// Tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+const alert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    alert('Mensaje enviado con éxito!!!', 'success')
+  })
+}
